a5cac43797098d73e9b5a82dfd555aef7fda66da
afern247 <>
2018-07-10 11:03:43 -0400

    sc

---
+++ routes/campgrounds.js
@@ -0,0 +1,116 @@
+var express = require("express");
+var router = express.Router();
+var Campground = require("../models/campground");
+
+
+// INDEX - Show all campgrounds
+router.get('/', function (req, res) {
+    // Get all campgrounds from DB
+    Campground.find({}, function (err, allCampgrounds) {
+        if (err) {
+            console.log(error('Error!\n'));
+            console.log(err);
+        } else {
+            //req.user gets the current user logged in, if no one, then null
+            res.render('campgrounds/index', { campgrounds: allCampgrounds });
+        }
+    });
+});
+
+// CREATE - Add new campground to db
+router.post('/', isLoggedIn, function (req, res) {
+    // get data from form and add to campgrounds array
+    var newName = req.body.name;
+    var newImage = req.body.image;
+    var newDescription = req.body.description;
+    var newAuthor = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {   // Store all the new campground info in that var so we can submit it later to DB
+        name: newName,
+        image: newImage,
+        description: newDescription,
+        author: newAuthor
+    };
+    // console.log(req.user); // Check which user is logged in, test
+    // Create a new campground and save to db
+    Campground.create(newCampground, function (err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            // redirect back to campground page
+            res.redirect('/campgrounds');
+        }
+    })
+});
+
+// NEW - Show form to create new campground
+router.get('/new', isLoggedIn, function (req, res) {
+    res.render('campgrounds/new');
+});
+
+
+// SHOW - shows more info about one campground
+router.get("/:id", function (req, res) {
+    //find the campground with provided ID, remeber  the comments are ID references, look at the comment model
+    Campground.findById(req.params.id).populate("comments").exec(function (err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            //console.log(foundCampground)
+            //render show template with that campground
+            res.render("campgrounds/show", {
+                campground: foundCampground
+            });
+        }
+    });
+});
+
+// EDIT CAMPGROUND
+router.get('/:id/edit', function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if (err) {
+            res.redirect('/campgrounds');
+        } else {
+            res.render('campgrounds/edit', {campground: foundCampground});
+        }
+    });
+});
+
+// UPDATE CAMPGROUND
+router.put('/:id', function (req, res) {
+    // find and update the correct campground
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function (err, updatedCampground) {
+        if (err) {
+            res.redirect('/campgrounds');
+        } else {
+            res.redirect('/campgrounds/' + req.params.id);
+        }
+    });
+});
+
+//DESTROY Campground
+router.delete('/:id', function (req, res) {
+    Campground.findByIdAndRemove(req.params.id, function(err){
+       if (err) {
+           res.redirect('/campgrounds');
+       } else {
+        res.redirect('/campgrounds');
+       }
+    });
+});
+
+//middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
+
+// export router to use it later on app.js:
+// var campgroundRoutes = require('./routes/campgrounds');
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,73 @@
+var express = require("express");
+var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
+
+
+
+//root route
+router.get('/', function (req, res) {
+    res.render('landing');
+});
+
+
+//show register form
+router.get('/register', function (req, res) {
+    res.render('register');
+});
+
+// handle sign up logic
+router.post('/register', function (req, res) {
+    var newUser = new User({
+        username: req.body.username
+    });
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/campgrounds');
+        });
+    });
+});
+
+//show login form
+router.get('/login', function (req, res) {
+    res.render('login');
+});
+
+// handle login logic
+// router.post ('/login', middleware, callback)
+router.post('/login', passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}), function (req, res) {});
+
+
+// log out route
+router.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+
+
+// middleware to check if user is logged in before he can add/see something
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
+/*
+//The 404 Route (ALWAYS Keep this as the last route)
+router.get('*', function (req, res) {
+    res.render('404');
+});
+
+*/
+
+module.exports = router;
